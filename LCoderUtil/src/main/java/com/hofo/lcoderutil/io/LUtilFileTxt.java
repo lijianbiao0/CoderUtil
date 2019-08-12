b42d1296e7295e93a6f3fee996e350704c81dc5f
lijianbiao <lijianbiao0@163.com>
2019-08-12 13:23:35 +0800

    增加文件管理类

--- LCoderUtil/src/main/java/com/hofo/lcoderutil/io/LUtilFileTxt.java
+++ LCoderUtil/src/main/java/com/hofo/lcoderutil/io/LUtilFileTxt.java
@@ -1,6 +1,9 @@
 package com.hofo.lcoderutil.io;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class LUtilFileTxt {
     /**
@@ -18,4 +21,27 @@ public class LUtilFileTxt {
         return file.delete();
     }
 
+    public static void append(String filePath, String message) {
+        append(filePath, message, true);
+    }
+
+    /**
+     * 追加文本文件
+     *
+     * @param filePath 文件路径
+     * @param message  追加内容
+     * @param append   是否追加
+     */
+    public static void append(String filePath, String message, boolean append) {
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(new FileWriter(filePath, append));
+            pw.append(message);
+            pw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            LUtilStreamUtil.closeStream(pw);
+        }
+    }
 }
